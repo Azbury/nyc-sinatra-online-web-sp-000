6bcb8cf2c2614cdda0b5212dc2b90d10c1e2a42a
Austin Asbury <acasbury25@aol.com>
2019-12-11 02:42:04 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,6 +6,16 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+    @figure = Figure.create(params['figure'])
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
 
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
   end
 end

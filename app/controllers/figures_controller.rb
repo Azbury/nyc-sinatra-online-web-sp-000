91204051a0e2b58b609ef0889c1f6abe1673146b
Austin Asbury <acasbury25@aol.com>
2019-12-11 02:42:40 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,11 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
   get '/figures/new' do
     @landmarks = Landmark.all
     erb :'figures/new'
